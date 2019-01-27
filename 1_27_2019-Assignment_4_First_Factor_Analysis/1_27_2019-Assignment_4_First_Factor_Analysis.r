033948b7c8e2e5241738db1bcd9339572f2f7ad0
Will Eddy <email@willeddy.com>
2019-01-27 11:41:39 -0500

    Initial commit, with first homework assignment

---
+++ 1_27_2019-Assignment_4_First_Factor_Analysis/1_27_2019-Assignment_4_First_Factor_Analysis.r
@@ -0,0 +1,11 @@
+# 1. Clean the Data
+#
+# 2. Are any of the variables reversed scored? Run a 1-factor EFA on each of the factors to ascertain if
+# they are or are not. Recode any that are not and then rerun the EFA to ensure that everything is aligned correctly.
+#
+# 3. Calculate the internal consistency of each of the following variables: Job performance,
+# competitive psychological climate, work-role behaviors, psychological bullying, perceived
+# incivility behaviors, work incivility scale, work stress, turnover intentions
+# 4. Do all of the scales demonstrate appropriate internal consistency?
+# 5. Can any of the scales be improved?
+
