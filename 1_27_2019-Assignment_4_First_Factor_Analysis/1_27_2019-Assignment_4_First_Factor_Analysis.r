5fa5f59673ebf52ace69a52caf76f836f7701a1d
willeddy <adwacz@gmail.com>
2019-01-28 17:55:45 -0500

    Reverse coded where necessary. Conducted reliability analyses on all constructs.

--- 1_27_2019-Assignment_4_First_Factor_Analysis/1_27_2019-Assignment_4_First_Factor_Analysis.r
+++ 1_27_2019-Assignment_4_First_Factor_Analysis/1_27_2019-Assignment_4_First_Factor_Analysis.r
@@ -1,46 +1,55 @@
-# 1. Clean the Data 
-# DONE
-#
-# 2. Are any of the variables reversed scored? Run a 1-factor EFA on each of the factors to ascertain if
-# they are or are not. Recode any that are not and then rerun the EFA to ensure that everything is aligned correctly.
-# DONE
-#
-# 3. Calculate the internal consistency of each of the following variables: Job performance,
-# competitive psychological climate, work-role behaviors, psychological bullying, perceived
-# incivility behaviors, work incivility scale, work stress, turnover intentions
-#
-# 4. Do all of the scales demonstrate appropriate internal consistency?
-#
-# 5. Can any of the scales be improved?
+
 
 DATA <- read.csv("Assignment_4_Data.csv", header=TRUE, na.strings="-999")
 colnames(DATA)[1] <- "Age"
-View(DATA)
 
 library(psych)
-library(hemp)
 
-# Splitting off CPC items, calculating chronbach's alpha
-# CPC <- DATA[c("CPC1", "CPC2", "CPC3", "CPC4")]
-# alpha(CPC)
-
-# Splitting off WRB items, running factor analysis on them
-# WRB <- DATA[c("WRB1", "WRB2", "WRB3", "WRB4", "WRB5", "WRB6", "WRB7")]
-# alpha(WRB)
-
-# Splitting off CPC items, running factor analysis on them
-# JP <- DATA[c("JP1", "JP2", "JP3")]
-# alpha(JP)
+# After identifying the variables that are reverse scored, I recode them using the psych package
 
 # Checking for reverse-scored items using a reliability test
 alpha(DATA)
 
-DATA <- reverse.code(c(1,-1,-1,-1,1,-1,-1,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-               1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1), DATA)
+DATA_RC <- reverse.code(c(1,1,1,1,1,-1,-1,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                          1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1), DATA)
 
-alpha(DATA)
+DATA_RC <- as.data.frame(DATA_RC)
 
-# After identifying that the variables 
+View(DATA_RC)
+
+#Splitting off JP items, running factor analysis on them
+JP <- DATA_RC[c("JP1-", "JP2-", "JP3-")]
+alpha(JP)
+
+#Splitting off CPC items, calculating chronbach's alpha
+CPC <- DATA_RC[c("CPC1", "CPC2", "CPC3", "CPC4")]
+CPC <- as.data.frame(CPC)
+alpha(CPC)
+
+#Splitting off WRB items, running factor analysis on them
+WRB <- DATA_RC[c("WRB1", "WRB2", "WRB3", "WRB4", "WRB5", "WRB6", "WRB7")]
+alpha(WRB)
+
+#Splitting off PB items, running factor analysis on them
+PB <- DATA_RC[c("PB1", "PB2", "PB3", "PB4", "PB5", "PB6", "PB7", "PB8", "PB9", "PB10", "PB11", "PB12")]
+alpha(PB)
+
+#Splitting off PIB items, running factor analysis on them
+PIB <- DATA_RC[c("PIB1", "PIB2", "PIB3")]
+alpha(PIB)
+
+#Splitting off WIS items, running factor analysis on them
+WIS <- DATA_RC[c("WIS1", "WIS2", "WIS3", "WIS4", "WIS5", "WIS6", "WIS7", "WIS8", "WIS9", "WIS10", "WIS11")]
+alpha(WIS)
+
+#Splitting off WS items, running factor analysis on them
+WS <- DATA_RC[c("WS1", "WS2", "WS3", "WS4", "WS5", "WS6", "WS7", "WS8")]
+alpha(WIS)
+
+#Splitting off TI items, running factor analysis on them
+TI <- DATA_RC[c("TI1", "TI2", "TI3", "TI4")]
+alpha(WIS)
+
 
 # Sources utilized ####################################################
 #
