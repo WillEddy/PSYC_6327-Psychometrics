5591f9ddfc367f177d17253703ec49f1f4bd90a3
Will Eddy <email@willeddy.com>
2019-02-14 11:06:25 -0500

    Factor Analysis - Assignment 6 - Mostly done.

---
+++ 2_13_2019-Assignment_6_Factor_Analysis/Eddy_Factor_Analysis.r
@@ -0,0 +1,27 @@
+library(haven)
+library(psych)
+library(GPArotation)
+
+DATA <- read_sav("PSYC 6327 Factor Analysis Assignment Data.sav")
+
+# Messing with the labels
+# unname(DATA)
+# names(DATA) <- c()
+########################
+
+FA <- fa(DATA,6)
+FA
+
+kaiser(FA)
+
+scree(DATA)
+
+fa.parallel(DATA)
+
+# Hypothesized number of factors based on dataset labels is 6.
+# VSS function documentation suggests applying "more than hypothesized," so using function default of 8.
+# Default rotation - varimax
+
+VSS(DATA)
+
+
