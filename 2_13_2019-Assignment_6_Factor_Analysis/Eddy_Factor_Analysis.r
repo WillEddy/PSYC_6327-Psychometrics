8722bdab6bdfac87b7b6da578ba86bd3e6396488
willeddy <adwacz@gmail.com>
2019-02-19 15:22:50 -0500

    Completed assignment

--- 2_13_2019-Assignment_6_Factor_Analysis/Eddy_Factor_Analysis.r
+++ 2_13_2019-Assignment_6_Factor_Analysis/Eddy_Factor_Analysis.r
@@ -4,38 +4,42 @@ library(GPArotation)
 
 DATA <- read_sav("PSYC 6327 Factor Analysis Assignment Data.sav")
 
-# Messing with the labels
-# unname(DATA)
-# names(DATA) <- c()
-########################
-
-FA <- fa(DATA,6)
-FA
-
-kaiser(FA)
-
-SCREE <- scree(DATA)
+SCREE <- scree(DATA, pc=FALSE)
 SCREE
+typeof(SCREE)
 
-PA <- fa.parallel(DATA)
+PA <- fa.parallel(DATA, fa="fa")
 PA
 
-
-
-# Hypothesized number of factors based on dataset labels is 6.
-# VSS function documentation suggests applying "more than hypothesized," so using function default of 8.
+# Hypothesized number of factors based on scree plot is 3.
+# VSS function documentation suggests applying "more than hypothesized," so using 4.
 # Default rotation - varimax
 
 VSS(DATA, n=4)
 
-VSS(DATA, rotate="oblimin")
+VSS(DATA, n=4, rotate="oblimin")
+
+# Was getting an error that I found might be related to missing data in teh dataset.
+# Solved using this clean-up syntax:
 
 DATA <- DATA[complete.cases(DATA),]
 
-fa(DATA)
-COR_DATA <- cor(DATA)
-FA_DATA <- fa(COR_DATA, nfactors=3, rotate="oblimin", oblique.scores=TRUE, covar=TRUE, residuals=TRUE)
+# Creating correlation table for fa() function, and requesting matrices.
 
+COR_DATA <- cor(DATA)
+
+FA_DATA <- fa(COR_DATA, nfactors=3, rotate="varimax")
 fa.sort(FA_DATA)
 
-FA_DATA
+FA_DATA2 <- fa(COR_DATA, nfactors=2, rotate="oblimin")
+fa.sort(FA_DATA2)
+
+FA_DATA3 <- fa(COR_DATA, nfactors=3, rotate="oblimin")
+fa.sort(FA_DATA3)
+
+FA_DATA4 <- fa(COR_DATA, nfactors=7, rotate="oblimin")
+fa.sort(FA_DATA4)
+
+# Looking at survey item labels
+
+str(DATA, nchar.max=1000)
