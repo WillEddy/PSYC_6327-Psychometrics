1f1db362b5a31d347f97d443562ded97356fad04
willeddy <adwacz@gmail.com>
2019-04-19 16:33:11 -0400

    Later Iteration, near completion.

--- 4_11_2019 - Test Manual/MFDA_Test_Validation.R
+++ 4_11_2019 - Test Manual/MFDA_Test_Validation.R
@@ -4,143 +4,146 @@ library(haven)
 library(psych)
 library(GPArotation)
 library(readr)
+
 DATA_ALL <- read_csv("Data/EDDY_TestManualData_numeric_abridged.csv", 
                  na = "NA")
 
-DATA <- DATA_ALL[,1:28]
+#Removing incomplete cases
+DATA_ALL <- na.exclude(DATA_ALL)
+
+# Descriptive analysis
+
+DEMOG <- (DATA_ALL[29:37])
+DEMOG <- na.exclude(DEMOG)
+
+summary(DEMOG)
+histogram(DEMOG$Sex)
+
+
+#Testing internal consistency
+alpha(DATA_ALL[1:28])
+alpha(DATA_ALL[1:10])
+alpha(DATA_ALL[10:20])
+alpha(DATA_ALL[21:28])
+
+
+# Removing lower quality items to raise cronbach's alpha / omega
+DATA <- DATA_ALL[,c(2,4:6,8:14,16,17,20:24,27,28,29,34,35)]
+alpha (DATA[1:7])
+alpha (DATA[8:14])
+alpha (DATA[15:21])
+alpha (DATA[1:20])
+
+PA <- fa.parallel((DATA[1:20]), fa="fa")
+PA
+
+# Hypothesized number of factors based on scree plot is 4.
+# VSS function documentation suggests applying "more than hypothesized," so using 4.
+
+VSS(DATA, n=3, rotate="oblmin")
+
+
+# Creating correlation table for fa() function, and requesting matrices.
+#Removing incomplete cases
+DATA <- na.exclude(DATA)
+
+COR_DATA <- cor(DATA[1:20])
+
+
+# Bowler suggested method
+FA_DATA2 <- fa(COR_DATA, nfactors=2, rotate="oblmin", fm="pa")
+FA_DATA2
+fa.sort(FA_DATA2)
+# Extracts one factor of 7 items and one of 1 item
+# Subset dataset for just the items I'm keeping
+
+
+
+DATA <- DATA[,c("DA6","DA8","DA10","CO4","LC1","LC3","Age","Sex","Race")]
+DATA$Total <- rowSums(DATA[1:6])
+
+alpha(DATA[1:6])
+
 
 View(DATA)
 
-# Identifying Reverse Scored Items - look for negative loadings
-DATA <- DATA[complete.cases(DATA),]
-COR_DATA <- cor(DATA)
-Rev_Score_Ident <- fa(COR_DATA, nfactors=1, rotate="oblimin")
-fa.sort(Rev_Score_Ident)
-
-# Recoding data for reverse scored items
-DATA$"DA1"[DATA$"DA1"=="1"] <- "5_rev"
-DATA$"DA1"[DATA$"DA1"=="2"] <- "4_rev"
-DATA$"DA1"[DATA$"DA1"=="4"] <- "2"
-DATA$"DA1"[DATA$"DA1"=="5"] <- "1"
-DATA$"DA1"[DATA$"DA1"=="5_rev"] <- "5"
-DATA$"DA1"[DATA$"DA1"=="4_rev"] <- "4"
-DATA$"DA1" <- as.numeric(DATA$"DA1")
-
-DATA$"CO8"[DATA$"CO8"=="1"] <- "5_rev"
-DATA$"CO8"[DATA$"CO8"=="2"] <- "4_rev"
-DATA$"CO8"[DATA$"CO8"=="4"] <- "2"
-DATA$"CO8"[DATA$"CO8"=="5"] <- "1"
-DATA$"CO8"[DATA$"CO8"=="6"] <- "NA"
-DATA$"CO8"[DATA$"CO8"=="7"] <- "NA"
-DATA$"CO8"[DATA$"CO8"=="5_rev"] <- "5"
-DATA$"CO8"[DATA$"CO8"=="4_rev"] <- "4"
-DATA$"CO8" <- as.numeric(DATA$"CO8")
-
-DATA$"DA7"[DATA$"DA7"=="1"] <- "5_rev"
-DATA$"DA7"[DATA$"DA7"=="2"] <- "4_rev"
-DATA$"DA7"[DATA$"DA7"=="4"] <- "2"
-DATA$"DA7"[DATA$"DA7"=="5"] <- "1"
-DATA$"DA7"[DATA$"DA7"=="6"] <- "NA"
-DATA$"DA7"[DATA$"DA7"=="7"] <- "NA"
-DATA$"DA7"[DATA$"DA7"=="5_rev"] <- "5"
-DATA$"DA7"[DATA$"DA7"=="4_rev"] <- "4"
-DATA$"DA7" <- as.numeric(DATA$"DA7")
-
-DATA$"CO5"[DATA$"CO5"=="1"] <- "5_rev"
-DATA$"CO5"[DATA$"CO5"=="2"] <- "4_rev"
-DATA$"CO5"[DATA$"CO5"=="4"] <- "2"
-DATA$"CO5"[DATA$"CO5"=="5"] <- "1"
-DATA$"CO5"[DATA$"CO5"=="6"] <- "NA"
-DATA$"CO5"[DATA$"CO5"=="7"] <- "NA"
-DATA$"CO5"[DATA$"CO5"=="5_rev"] <- "5"
-DATA$"CO5"[DATA$"CO5"=="4_rev"] <- "4"
-DATA$"CO5" <- as.numeric(DATA$"CO5")
-
-DATA$"CO9"[DATA$"CO9"=="1"] <- "5_rev"
-DATA$"CO9"[DATA$"CO9"=="2"] <- "4_rev"
-DATA$"CO9"[DATA$"CO9"=="4"] <- "2"
-DATA$"CO9"[DATA$"CO9"=="5"] <- "1"
-DATA$"CO9"[DATA$"CO9"=="6"] <- "NA"
-DATA$"CO9"[DATA$"CO9"=="7"] <- "NA"
-DATA$"CO9"[DATA$"CO9"=="5_rev"] <- "5"
-DATA$"CO9"[DATA$"CO9"=="4_rev"] <- "4"
-DATA$"CO9" <- as.numeric(DATA$"CO9")
-
-# Begin factor analysis
-
-PA <- fa.parallel(DATA, fa="fa")
-PA
-
-# Hypothesized number of factors based on scree plot is 3.
-# VSS function documentation suggests applying "more than hypothesized," so using 4.
-# Default rotation - varimax
-
-VSS(DATA, n=6, rotate="oblimin")
-
-VSS(DATA, n=4, rotate="oblimin")
-
-# Was getting an error that I found might be related to missing data in the dataset.
-# Solved using this clean-up syntax:
-
-DATA <- DATA[complete.cases(DATA),]
-
-# Creating correlation table for fa() function, and requesting matrices.
-
-COR_DATA <- cor(DATA)
-
-
-FA_DATA2 <- fa(COR_DATA, nfactors=1, rotate="oblimin")
-fa.sort(FA_DATA2)
-
-# Bowler suggested method
-FA_DATA2 <- fa(COR_DATA, nfactors=2, rotate="Promax", fm="pa")
-0
-# Extracts one factor of 7 items and one of 1 item
-
-
-FA_DATA2 <- fa(COR_DATA, nfactors=3, rotate="oblimin")
-fa.sort(FA_DATA2)
-#Extracts 3 factors: one of 4 and two of 2
-# Factor 1: 28, 17, 11, 23 - conscientiousness
-# Factor 3: 6, 4 - lateral thinking ability
-# Factor 2: 5, 26 (reverse scored?) - rule following VS external locus of control
-
-
-
-FA_DATA2 <- fa(COR_DATA, nfactors=4, rotate="oblimin")
-fa.sort(FA_DATA2)
-#Extracts 4 factors of 2 items
-# Factor 4: 28, 10 - team value (conscientiousness?)
-# Factor 1: 17, 18 - self efficacy
-# Factor 3: 6, 4 - lateral thinking ability
-# Factor 2: 5, 26 (reverse scored?) - rule following VS external locus of control
-
-FA_DATA2 <- fa(COR_DATA, nfactors=5, rotate="oblimin")
-fa.sort(FA_DATA2)
-# 4 of 2, 1 of 1
-# Factor 4: 28, 10 - conscientiousness
-# Factor 1: 17, 18 - self efficacy
-# Factor 3: 6, 4 = lateral thinking ability
-# Factor 2: 22, 21 - Dispositional autonomy
-# Factor 5: 2
-
-
-FA_DATA2 <- fa(COR_DATA, nfactors=6, rotate="oblmin")
-fa.sort(FA_DATA2)
-# Factor 3: 6,4,3 - Lateral thinking
-# Factor 4: 28
-# Factor 1: 17, 18 - self efficacy
-# Factor 2: 21. 22 - Dispositional autonomy
-# Factor 6: X
-# Factor 5: 2
-
-# Subset dataset for just the items I'm keeping
-
-KEEPERS <- DATA[,c("LC1","CO4","DA6","DA8","LC4","LC7","DA10")]
-
-View(KEEPERS)
+omega(DATA)
 
 # Calculate Reliability estimates
 
 alpha(KEEPERS)
 omega(KEEPERS)
+
+DATA_Male = subset(DATA, DATA$Sex=="1")
+DATA_Female = subset(DATA, DATA$Sex=="2")
+t.test(DATA_Female$Total,DATA_Male$Total)
+
+DATA_White = subset(DATA, DATA$Race=="1")
+DATA_AfricanAmerican = subset(DATA, DATA$Race=="2")
+t.test(DATA_White$Total,DATA_AfricanAmerican$Total)
+
+DATA_18to24 = subset(DATA, DATA$Age=="2")
+DATA_25to34 = subset(DATA, DATA$Race=="3")
+t.test(DATA_18to24$Total,DATA_25to34$Total)
+
+View(DATA)
+
+head(DATA$Race)
+
+
+# Identifying Reverse Scored Items - look for negative loadings
+
+#COR_DATA <- cor(DATA)
+#Rev_Score_Ident <- fa(COR_DATA, nfactors=1, rotate="oblimin")
+#fa.sort(Rev_Score_Ident)
+
+# Recoding data for reverse scored items
+# DATA$"DA1"[DATA$"DA1"=="1"] <- "5_rev"
+# DATA$"DA1"[DATA$"DA1"=="2"] <- "4_rev"
+# DATA$"DA1"[DATA$"DA1"=="4"] <- "2"
+# DATA$"DA1"[DATA$"DA1"=="5"] <- "1"
+# DATA$"DA1"[DATA$"DA1"=="5_rev"] <- "5"
+# DATA$"DA1"[DATA$"DA1"=="4_rev"] <- "4"
+# DATA$"DA1" <- as.numeric(DATA$"DA1")
+# 
+# DATA$"CO8"[DATA$"CO8"=="1"] <- "5_rev"
+# DATA$"CO8"[DATA$"CO8"=="2"] <- "4_rev"
+# DATA$"CO8"[DATA$"CO8"=="4"] <- "2"
+# DATA$"CO8"[DATA$"CO8"=="5"] <- "1"
+# DATA$"CO8"[DATA$"CO8"=="6"] <- "NA"
+# DATA$"CO8"[DATA$"CO8"=="7"] <- "NA"
+# DATA$"CO8"[DATA$"CO8"=="5_rev"] <- "5"
+# DATA$"CO8"[DATA$"CO8"=="4_rev"] <- "4"
+# DATA$"CO8" <- as.numeric(DATA$"CO8")
+# 
+# DATA$"DA7"[DATA$"DA7"=="1"] <- "5_rev"
+# DATA$"DA7"[DATA$"DA7"=="2"] <- "4_rev"
+# DATA$"DA7"[DATA$"DA7"=="4"] <- "2"
+# DATA$"DA7"[DATA$"DA7"=="5"] <- "1"
+# DATA$"DA7"[DATA$"DA7"=="6"] <- "NA"
+# DATA$"DA7"[DATA$"DA7"=="7"] <- "NA"
+# DATA$"DA7"[DATA$"DA7"=="5_rev"] <- "5"
+# DATA$"DA7"[DATA$"DA7"=="4_rev"] <- "4"
+# DATA$"DA7" <- as.numeric(DATA$"DA7")
+# 
+# DATA$"CO5"[DATA$"CO5"=="1"] <- "5_rev"
+# DATA$"CO5"[DATA$"CO5"=="2"] <- "4_rev"
+# DATA$"CO5"[DATA$"CO5"=="4"] <- "2"
+# DATA$"CO5"[DATA$"CO5"=="5"] <- "1"
+# DATA$"CO5"[DATA$"CO5"=="6"] <- "NA"
+# DATA$"CO5"[DATA$"CO5"=="7"] <- "NA"
+# DATA$"CO5"[DATA$"CO5"=="5_rev"] <- "5"
+# DATA$"CO5"[DATA$"CO5"=="4_rev"] <- "4"
+# DATA$"CO5" <- as.numeric(DATA$"CO5")
+# 
+# DATA$"CO9"[DATA$"CO9"=="1"] <- "5_rev"
+# DATA$"CO9"[DATA$"CO9"=="2"] <- "4_rev"
+# DATA$"CO9"[DATA$"CO9"=="4"] <- "2"
+# DATA$"CO9"[DATA$"CO9"=="5"] <- "1"
+# DATA$"CO9"[DATA$"CO9"=="6"] <- "NA"
+# DATA$"CO9"[DATA$"CO9"=="7"] <- "NA"
+# DATA$"CO9"[DATA$"CO9"=="5_rev"] <- "5"
+# DATA$"CO9"[DATA$"CO9"=="4_rev"] <- "4"
+# DATA$"CO9" <- as.numeric(DATA$"CO9")
+
+# Begin factor analysis
